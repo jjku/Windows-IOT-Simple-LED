3de3923de7372f931a39250e0ef6767c3fa53a4e
Jeff Ku <jjku@accumark.ca>
2017-04-25 10:31:20 -0400

    Add project files.

---
+++ AccessGPIO/SetGPIOHigh.cs
@@ -0,0 +1,42 @@
+﻿using Windows.Devices.Gpio;
+
+namespace AccessGPIO
+{
+    public class SetGPIOHigh
+    {
+
+        GpioController gpio;
+
+        public void GPIO(int _pinID)
+        {
+            gpio = GpioController.GetDefault();
+            if (gpio == null)
+                return; // GPIO not available on this system
+
+            // int _pinIDint = int.Parse(_pinID.ToString());
+
+            using (GpioPin pin = gpio.OpenPin(_pinID))
+            {
+                // Latch HIGH value first. This ensures a default value when the pin is set as output
+                pin.Write(GpioPinValue.High);
+
+                // Set the IO direction as output
+                pin.SetDriveMode(GpioPinDriveMode.Output);
+            } // Close pin - will revert to its power-on state
+
+            
+        }
+
+        public void GPIOLow(int _pinID)
+        {
+
+            GpioPin pin = gpio.OpenPin(_pinID);
+            pin.Write(GpioPinValue.Low);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+        }
+
+
+
+    }
+}
---
+++ EnableLED/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using AccessGPIO;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EnableLED
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SetGPIOHigh GPIO;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            GPIO = new SetGPIOHigh();
+            GPIO.GPIO(4);
+
+        }
+
+        public void _setGPIOHigh(int _pinNum)
+        {
+            
+        }
+
+        private void Button_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            GPIO.GPIOLow(4);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GPIO.GPIOLow(4);
+        }
+    }
+}
